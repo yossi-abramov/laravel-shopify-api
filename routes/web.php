feee434aa141ee638071a6581e52cac51eefae4a
yossi-abramov <info@studio-nmza.com>
2021-07-22 02:23:32 +0300

    added routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrdersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/orders', [OrdersController::class, 'index']);
+Route::get('/orders/{order_id}', [OrdersController::class, 'getOrderItems']);
